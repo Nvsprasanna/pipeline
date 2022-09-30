2c6e0c0035c4cfe921cf252d4558ecb457e648a5
Neeraja Rongali <neerajarongali@Neerajas-MacBook-Air.local>
2022-09-30 16:33:00 +0530

    multi server deployment

--- firstpipeline.groovy
+++ firstpipeline.groovy
@@ -5,13 +5,13 @@ pipeline {
     parameters {  string(name: 'MYJOB_NAME', defaultValue: 'firstpipeline', description: '')
                   string(name: 'MYSOURCE_BRANCH', defaultValue: 'master', description: '')
                   string(name: 'MYBUILD_NUMBER', defaultValue: '10', description: '')
-                  string(name: 'ip', defaultValue: '', description: 'Enter IP Value') 
+                  choices(name: 'ip', choices: ['QA,3.85.91.150','Dev,3.95.229.196', 'Prod,18.234.225.172'], description: 'select IP Value') 
                }
     
     stages {
         stage('checkout Stage') {
             steps {
-            checkout([$class: 'GitSCM', branches: [[name: '*/$SOURCE_BRANCH']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/Nvsprasanna/Sep22-code.git']]])
+            checkout([$class: 'GitSCM', branches: [[name: '*/$MYSOURCE_BRANCH']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/Nvsprasanna/Sep22-code.git']]])
                 sh '''ls -la'''
 
             }
@@ -40,8 +40,16 @@ pipeline {
             steps {
                 echo 'we are in  deploymnet step'
                 sh ''' echo "$MYJOB_NAME, $MYSOURCE_BRANCH, $MYBUILD_NUMBER"
+                IFS=',' read -ra myArray <<< "$myip"
+                for ip in "${myArray[@]}"
+                do
+                    if [ $ip == ${myArray[0]} ]; then 
+                        echo "You first value is not IP ignored it"
+                    else
                 aws s3 cp s3://nvsbucket/$MYJOB_NAME/$MYSOURCE_BRANCH/$MYBUILD_NUMBER/hello-$MYBUILD_NUMBER.war .
                 scp -o StrictHostKeyChecking=no -i /tmp/Linuxserver.pem hello-$MYBUILD_NUMBER.war ec2-user@$ip:/var/lib/tomcat/webapps
+                fi
+                done
                 '''
             }
         }
