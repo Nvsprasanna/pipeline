2adc639db208e4d55dbc320bb3a661d5dde7f3fa
Neeraja Rongali <neerajarongali@Neerajas-MacBook-Air.local>
2022-09-30 17:31:54 +0530

    add

--- privategit.groovy
+++ privategit.groovy
@@ -10,31 +10,13 @@ pipeline {
         stage('checkout Stage') {
 
             steps {
-            checkout([$class: 'GitSCM', branches: [[name: '*/$MYSOURCE_BRANCH']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'jenkins-user-github',url: 'https://github.com/Nvsprasanna/myprivaterepo.git']]])
+            checkout([$class: 'GitSCM', branches: [[name: '*/$MYSOURCE_BRANCH']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'root',url: 'https://github.com/Nvsprasanna/myprivaterepo.git']]])
                 sh '''ls -la'''
+                echo " We are connected in private source code"
 
             }
         }
-        stage('Build stage') {
-            steps {
-                echo 'we are in builld statge'
-                sh ''' mvn clean package 
-                ls -la target/ '''
-
-            }
-        }
-        stage('Upload stage') {
-            steps {
-                echo 'we are in upload statge'
-                sh ''' aws s3 cp target/hello-*.war s3://nvsbucket/$JOB_NAME/$SOURCE_BRANCH/$BUILD_NUMBER/ '''
-            }
-        }
-        stage('Artifcat check') {
-            steps {
-                echo 'we are in  statge'
-                sh ''' aws s3 ls s3://nvsbucket/$JOB_NAME/$SOURCE_BRANCH/$BUILD_NUMBER/ '''
-            }
-        }
+        
         
     }
 }
