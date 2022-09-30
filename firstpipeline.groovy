060838fa35ed246dca049e6fffbd0ac818dabd6c
Neeraja Rongali <neerajarongali@Neerajas-MacBook-Air.local>
2022-09-30 16:35:12 +0530

    multi server deployment1

--- firstpipeline.groovy
+++ firstpipeline.groovy
@@ -5,11 +5,12 @@ pipeline {
     parameters {  string(name: 'MYJOB_NAME', defaultValue: 'firstpipeline', description: '')
                   string(name: 'MYSOURCE_BRANCH', defaultValue: 'master', description: '')
                   string(name: 'MYBUILD_NUMBER', defaultValue: '10', description: '')
-                  choices(name: 'ip', choices: ['QA,3.85.91.150','Dev,3.95.229.196', 'Prod,18.234.225.172'], description: 'select IP Value') 
+                  choice(name: 'ip', choices: ['QA,3.85.91.150','Dev,3.95.229.196', 'Prod,18.234.225.172'], description: 'select IP Value') 
                }
     
     stages {
         stage('checkout Stage') {
+
             steps {
             checkout([$class: 'GitSCM', branches: [[name: '*/$MYSOURCE_BRANCH']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/Nvsprasanna/Sep22-code.git']]])
                 sh '''ls -la'''
