5256bfed6f27d0ec75a76eca961a1d8cdce747fd
Neeraja Rongali <neerajarongali@Neerajas-MacBook-Air.local>
2022-09-30 14:41:48 +0530

    Adding pipeline file

---
+++ firstpipeline.groovy
@@ -0,0 +1,28 @@
+Firstpipeline.groovy
+
+pipeline {
+    agent any
+
+    stages {
+        stage('checkout Stage') {
+            steps {
+                echo 'we are in checkout statge'
+            }
+        }
+        stage('Build stage') {
+            steps {
+                echo 'we are in builld statge'
+            }
+        }
+        stage('Upload stage') {
+            steps {
+                echo 'we are in upload statge'
+            }
+        }
+        stage('Artifcat check') {
+            steps {
+                echo 'we are in  statge'
+            }
+        }
+    }
+}
