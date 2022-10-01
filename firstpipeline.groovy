01feeb8d277f864ee7a57b8a2368f236b2cf53bc
Neeraja Rongali <neerajarongali@Neerajas-MacBook-Air.local>
2022-10-01 07:17:59 +0530

    build job

--- firstpipeline.groovy
+++ firstpipeline.groovy
@@ -2,10 +2,9 @@
 
 pipeline {
     agent any
-    parameters {  string(name: 'MYJOB_NAME', defaultValue: 'firstpipeline', description: '')
+    parameters { 
                   string(name: 'MYSOURCE_BRANCH', defaultValue: 'master', description: '')
-                  string(name: 'MYBUILD_NUMBER', defaultValue: '10', description: '')
-                  choice(name: 'ip', choices: ['QA,3.85.91.150','Dev,3.95.229.196', 'Prod,18.234.225.172'], description: 'select IP Value') 
+                  choice(name: 'myip', choices: ['QA,172.31.92.239','Dev,172.31.88.212', 'Prod,172.31.80.36'], description: 'select IP Value') 
                }
     
     stages {
@@ -37,22 +36,6 @@ pipeline {
                 sh ''' aws s3 ls s3://nvsbucket/$JOB_NAME/$SOURCE_BRANCH/$BUILD_NUMBER/ '''
             }
         }
-        stage('Deployment step') {
-            steps {
-                echo 'we are in  deploymnet step'
-                sh ''' echo "$MYJOB_NAME, $MYSOURCE_BRANCH, $MYBUILD_NUMBER"
-                IFS=',' read -ra myArray <<< "$myip"
-                for ip in "${myArray[@]}"
-                do
-                    if [ $ip == ${myArray[0]} ]; then 
-                        echo "You first value is not IP ignored it"
-                    else
-                aws s3 cp s3://nvsbucket/$MYJOB_NAME/$MYSOURCE_BRANCH/$MYBUILD_NUMBER/hello-$MYBUILD_NUMBER.war .
-                scp -o StrictHostKeyChecking=no -i /tmp/Linuxserver.pem hello-$MYBUILD_NUMBER.war ec2-user@$ip:/var/lib/tomcat/webapps
-                fi
-                done
-                '''
-            }
-        }
+        
     }
 }
