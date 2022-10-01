9d86db94c8c3c7c150c984bf5e2bcf309504d92f
Neeraja Rongali <neerajarongali@Neerajas-MacBook-Air.local>
2022-10-01 07:50:45 +0530

    adding parametere to trigger21

--- firstpipeline.groovy
+++ firstpipeline.groovy
@@ -41,7 +41,7 @@ pipeline {
     build job: 'pipeline_deploy', parameters: [[$class: 'StringParameterValue', name: 'MYJOB_NAME', value: "${JOB_NAME}"],
                                               [$class: 'StringParameterValue', name: 'MYSOURCE_BRANCH', value: "${MYSOURCE_BRANCH}"],
                                               [$class: 'StringParameterValue', name: 'MYBUILD_NUMBER', value: "${BUILD_NUMBER}"],
-                                              [$class: 'ExtendedChoiceParameterValue', name: 'myip', value: "${myip}"]
+                                               choice(name: 'Choicemyip', value: "${myip}")
                                               ]
                        }
 }
