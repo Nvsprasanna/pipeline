81527d33cbf335bdf340b4a378f90627b6cd7440
Neeraja Rongali <neerajarongali@Neerajas-MacBook-Air.local>
2022-10-01 07:57:22 +0530

    private git hub

--- privategit.groovy
+++ privategit.groovy
@@ -10,7 +10,7 @@ pipeline {
         stage('checkout Stage') {
 
             steps {
-            checkout([$class: 'GitSCM', branches: [[name: '*/$MYSOURCE_BRANCH']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'Nvsprasanna',url: 'https://github.com/Nvsprasanna/myprivaterepo.git']]])
+            checkout([$class: 'GitSCM', branches: [[name: '*/$MYSOURCE_BRANCH']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'jenkins-user-github',url: 'https://github.com/Nvsprasanna/myprivaterepo.git']]])
                 sh '''ls -la'''
                 echo " We are connected in private source code"
 
